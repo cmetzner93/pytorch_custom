2aacd5ba1b42e759d3d0d5029da688ef0560fddb
cmetzner93 <cmetzner93@gmail.com>
2024-06-18 21:03:36 -0400

    docs - added doc string to function 'compute_performance_metrics'

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ This file contains source code for the utility function used in this codebase.
 # built-in
 import os
 import sys
-from typing import Dict
+from typing import Dict, Union
 
 # installed
 import numpy as np
@@ -19,13 +19,45 @@ from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_sc
 def compute_performance_metrics(
     y_trues: np.array,
     y_preds: np.array,
-    y_probs: np.array = None,
     num_classes: int
-    ) -> Dict[str, float]:
+    ) -> Dict[str, Union[float, np.array]]:
+    """
+    This function computes multiple performance metrics to evaluate a machine 
+    learning model.
 
+    Parameters
+    ----------
+    y_trues : np.array
+        Array containing the ground-truth labels corresponding to the samples
+    y_preds : np.array
+        Array containing the predictions correspendoning to the samples
+    num_classes : int
+        Number of classes in the label space
+
+    Return
+    ------
+    Dict[str, Union[float, np.array]]
+        Dictionary containing the results the various performance metrics 
+    """
     # F1-scores
-    f1_macro = f1_score(y_true=y_trues, y_pred=y_preds, average='macro', labels=np.arange(0, num_classes))
-    f1_micro = f1_score(y_true=y_trues, y_pred=y_preds, average='micro', labels=np.arange(0, num_classes))
+    f1_macro = f1_score(
+        y_true=y_trues,
+        y_pred=y_preds,
+        average='macro',
+        labels=np.arange(0, num_classes)
+    )
+    f1_micro = f1_score(
+        y_true=y_trues,
+        y_pred=y_preds,
+        average='micro',
+        labels=np.arange(0, num_classes)
+    )
+    f1_ind = f1_score(
+        y_true=y_trues,
+        y_pred=y_preds,
+        average=None,
+        labels=np.arange(0, num_classes)
+    )
 
     accuracy = accuracy_score(y_true=y_trues, y_pred=y_preds, normalize=True)
 
