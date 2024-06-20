1d1db7ea6b4227601a61d1c9ea783f62ebcf0ce0
cmetzner93 <cmetzner93@gmail.com>
2024-06-19 22:28:06 -0400

    feat implement function train_one_epoch with scaler and autocast

--- src/training.py
+++ src/training.py
@@ -28,11 +28,18 @@ class Trainer:
         train_kwargs: Dict[str, Union[int, float, bool]],
         paths_dict: Dict[str, str],
         ddp_training: bool = False,
-
+        device: str = None,
     )
     self._model = model
     self._paths_dict = paths_dict
     self._ddp_training = ddp_training
+    self._device = device
+
+    # Initialize optimizer, scaler, scheduler, and loss function
+    self._optimizer = torch.optim.AdamW(self._model.parameters(), lr=train_kwargs['learning_rate'], betas=(0.9, 0.999))
+    self._scheduler = torch.optim.lr_scheduler.LinearLR(optimizer=self._optimizer, total_iters=5)
+    self._scaler = torch.cuda.amp.GradScaler(enabled=True)
+    self._loss_fct = torch.nn.CrossEntropyLoss()
     
     # Initialize early stopping parameters
     self._best_val_loss = np.inf
@@ -73,6 +80,33 @@ class Trainer:
             if early_stopping:
                 break
 
+    def _train_one_epoch(self, train_loader) -> torch.Tensor:
+        for b, batch in enumerate(train_loader):
+            if self._debugging:
+                if b + 1 == 2:
+                    break
+
+            X = batch['X'].to(self._device, non_blocking=True)
+            Y = batch['Y'].to(self._device, non_blocking=True)
+            # A = batch['A'].to(self._device, non_blocking=True)  # Transformer Model
+
+           with torch.cuda.amp.autocast(enabled=True):
+                if self._model_type in ['CLF']:
+                    logits = self._model(X, A)
+                else:
+                    logits = self._model(X)
+
+                train_loss = self._loss_fct(logits, Y)
+            self._scaler.scale(train_loss).backward()
+
+            self._scaler.step(self._optimizer)
+            self._scaler.update()
+            self._optimizer.zero_grad(set_to_none=True)
+        return train_loss.detach().cpu().numpy()
+
+
+
+
     def _early_stopping(self, val_loss: torch.Tensor) -> bool:
         if val_loss < self._best_val_loss:
             self._best_val_loss = val_loss
@@ -85,8 +119,6 @@ class Trainer:
                 print('Apply early stopping of model training', flush=True)
                 return True
         return False
-
-
 
 
 
