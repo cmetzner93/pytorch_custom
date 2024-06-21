875065d0561c70253885d20a10b0e560e480c59b
cmetzner93 <cmetzner93@gmail.com>
2024-06-20 22:02:12 -0400

    removed dummy creator function from file

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,6 @@ This file contains source code for the utility function used in this codebase.
     @date: 06/17/2024
     @last modified: 06/20/2024
 """
-
 # Load libraries
 # built-in
 import os
@@ -15,7 +14,6 @@ from typing import Dict, Union, List
 import numpy as np
 import pandas as pd
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
-import torch
 
 def compute_performance_metrics(
     y_trues: np.array,
@@ -70,39 +68,4 @@ def compute_performance_metrics(
     }
 
     return metrics
-
-
-def create_dummy_dataset(
-    N: Union[int, List[int, int, int]],
-    path_save_data: str
-) -> None:
-    """
-    This function creates a dummy dataset containing samples as integer-index
-    sequences annotated with either one of three classes (0, 1, 2). This
-    dataset is meant to simulate a multi-class text classification task.
-
-    Parameters
-    ----------
-    N : Union[int, List[int, int, int]]
-        Number of samples in each split (i.e., training, validation, testing)
-        If N: int all split have the same number of samples
-        If N: List[int, int, int] each split can have a different number of
-        samples.
-    path_save_data : str
-        Absolute path to storage location for dummy dataset.
-
-    """
-    if is_instance(N, int):
-        N = [N] * 3
-    elif is_instance(N, list):
-        assert len(N) == 3
-
-    splits = ['train', 'val', 'test']
-    for split, n in zip(splits, N):
-        X = torch.randint(0, 20000, (3000, ))
-        Y = torch.randint(0, 3, (n, ))
-
-        df = pd.DataFrame.from_dict({'X': X, 'Y': Y})
-        df.to_parquet(os.path.join(path_save_data, f'dummy_{split}.parquet'))
-
 
